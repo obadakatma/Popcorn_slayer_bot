fe5ff7ae557d5f857663fe109531a1ed594d1a09
obada katma <104722024+obadakatma@users.noreply.github.com>
2023-09-07 18:35:57 +0300

    Delete apitest.py

--- apitest.py
+++
@@ -1,8 +0,0 @@
-import requests
-import json
-import os
-apikey = os.getenv("APIKEY")
-x = requests.get(f'http://api.themoviedb.org/3/search/tv?api_key={apikey}&query="how i met your mother"')
-file = open("data.json", "w")
-json.dump(x.json(), file, indent=6)
-file.close()
